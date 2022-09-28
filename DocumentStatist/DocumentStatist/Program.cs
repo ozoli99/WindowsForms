ac281ee24dc00d8c64fcfea8667bf1faf0a2f96d
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 23:34:32 +0200

    Add word count section to program.cs

--- DocumentStatist/DocumentStatist/Program.cs
+++ DocumentStatist/DocumentStatist/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DocumentStatist
+﻿using DocumentStatist.Model;
+
+namespace DocumentStatist
 {
     class Program
     {
@@ -30,6 +32,61 @@
             Console.WriteLine($"Proper noun count: {stat.ProperNounCount}");
             Console.WriteLine($"Coleman-Lieu index: {stat.ColemanLieuIndex:f2}");
             Console.WriteLine($"Flesch Reading Ease: {stat.FleschReadingEase:f2}");
+
+            // WORD COUNT
+
+            Console.WriteLine();
+            Console.WriteLine("Parameters for word counting");
+            int minLength = ReadPositive("Minimum word length: ");
+            int minOccurence = ReadPositive("Minimum word occurence: ");
+
+            Console.WriteLine();
+            Console.WriteLine("Ignored words (separated by comma):");
+            var ignoredWords = new List<string>();
+            bool success = false;
+            do
+            {
+                try
+                {
+                    string line = Console.ReadLine() ?? string.Empty;
+
+                    ignoredWords = line.Split(',')
+                        .Select(w => w.Trim().ToLower())
+                        .ToList();
+                    success = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect format, try again!");
+                }
+            } while (!success);
+
+            var pairs = stat.DistinctWordCount
+                .Where(p => p.Value >= minOccurence)
+                .Where(p => p.Key.Length >= minLength)
+                .Where(p => !ignoredWords.Contains(p.Key))
+                .OrderByDescending(p => p.Value);
+
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+
+            return 0;
+        }
+
+        static int ReadPositive(string message)
+        {
+            int number;
+            bool success;
+
+            do
+            {
+                Console.Write(message);
+                success = Int32.TryParse(Console.ReadLine(), out number) && number > 0;
+            } while (!success);
+
+            return number;
         }
     }
 }
