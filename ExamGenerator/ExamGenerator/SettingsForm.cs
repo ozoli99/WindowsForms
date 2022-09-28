4fc94f83583a56732161f7d93f0a35d9c69762a1
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 13:16:26 +0200

    Implement constructor of SettingsForm

--- ExamGenerator/ExamGenerator/SettingsForm.cs
+++ ExamGenerator/ExamGenerator/SettingsForm.cs
@@ -34,9 +34,30 @@ namespace ExamGenerator
 
         #endregion
 
-        public SettingsForm()
+        #region Constructor
+
+        /// <summary>
+        /// Beallitasok kezeloablakanak peldanyositasa.
+        /// </summary>
+        /// <param name="questionCount">Tetelek szama.</param>
+        /// <param name="periodCount">Periodushossz.</param>
+        /// <param name="historyList">Korabbi tetelek listaja.</param>
+        public SettingsForm(int questionCount, int periodCount, List<int> historyList)
         {
             InitializeComponent();
+
+            _numericQuestionCount.Value = questionCount;
+            _numericPeriodLength.Value = periodCount;
+            _historyList = historyList;
+
+            for (int i = 1; i < _numericQuestionCount.Value; i++)
+            {
+                _checkedListBox.Items.Add(i, !_historyList.Contains(i));
+            }
+
+            _numericQuestionCount.ValueChanged += new EventHandler(NumericQuestionCount_ValueChanged);
         }
+
+        #endregion
     }
 }
