88cf5ac909dad7cab15fb46d6457cfd126c56fde
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 12:20:57 +0200

    Add private fields and implement constructor of CalculatorForm

--- Calculator/Calculator/CalculatorForm.Designer.cs
+++ Calculator/Calculator/CalculatorForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    partial class Form1
+    partial class CalculatorForm
     {
         /// <summary>
         ///  Required designer variable.
--- Calculator/Calculator/CalculatorForm.cs
+++ Calculator/Calculator/CalculatorForm.cs
@@ -1,10 +1,36 @@
 namespace Calculator
 {
-    public partial class Form1 : Form
+    /// <summary>
+    /// Muveletek felsorolasi tipusa.
+    /// </summary>
+    public enum Operation { None, Add, Substract, Multiply, Divide }
+
+    /// <summary>
+    /// Szamologep ablak tipusa.
+    /// </summary>
+    public partial class CalculatorForm : Form
     {
-        public Form1()
+        #region Private fields
+
+        private Double _result;
+        private Operation _operation;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Szamologep ablak peldanyositasa.
+        /// </summary>
+        public CalculatorForm()
         {
             InitializeComponent();
+
+            _operation = Operation.None;
+            _result = 0;
+            _textNumber.Text = "0";
         }
+
+        #endregion
     }
 }
