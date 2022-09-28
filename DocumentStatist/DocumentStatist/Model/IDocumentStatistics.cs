4d9a129f2d59185e354193d6a03179af4017afb6
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 22:40:44 +0200

    Create IDocumentStatistics interface

---
+++ DocumentStatist/DocumentStatist/Model/IDocumentStatistics.cs
@@ -0,0 +1,15 @@
+﻿namespace DocumentStatist.Model
+{
+    public interface IDocumentStatistics
+    {
+        IDictionary<string, int> DistinctWordCount { get; }
+        int CharacterCount { get; }
+        int NonWhiteSpaceCharacterCount { get; }
+        int SentenceCount { get; }
+        int ProperNounCount { get; }
+        double ColemanLieuIndex { get; }
+        double FleschReadingEase { get; }
+
+        void Load();
+    }
+}
