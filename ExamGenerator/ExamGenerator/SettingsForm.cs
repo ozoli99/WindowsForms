6c04ce362c07a43e48ea5356c1e1898935732b39
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 13:12:43 +0200

    Add private fields and public properties to SettingsForm

--- ExamGenerator/ExamGenerator/SettingsForm.cs
+++ ExamGenerator/ExamGenerator/SettingsForm.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace ExamGenerator
 {
+    /// <summary>
+    /// Beallitasok kezeloablakanak tipusa.
+    /// </summary>
     public partial class SettingsForm : Form
     {
+        #region Private fields
+
+        private List<int> _historyList;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Tetelek szamanak lekerdezese.
+        /// </summary>
+        public int QuestionCount
+        { 
+            get { return Convert.ToInt32(_numericQuestionCount.Value); }
+        }
+
+        /// <summary>
+        /// Periodus hosszanak lekerdezese.
+        /// </summary>
+        public int PeriodLength
+        { 
+            get { return Convert.ToInt32(_numericPeriodLength.Value); }
+        }
+
+        #endregion
+
         public SettingsForm()
         {
             InitializeComponent();
