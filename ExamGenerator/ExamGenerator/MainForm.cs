3a5cef6c3f926dcaddf3ce8676e70e20541dfaf4
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-28 12:54:24 +0200

    Add private fields to MainForm

--- ExamGenerator/ExamGenerator/MainForm.cs
+++ ExamGenerator/ExamGenerator/MainForm.cs
@@ -1,7 +1,20 @@
 namespace ExamGenerator
 {
+    /// <summary>
+    /// Foablak tipusa.
+    /// </summary>
     public partial class MainForm : Form
     {
+        #region Private fields
+
+        private System.Windows.Forms.Timer _timer;
+        private Random _questionGenerator;
+        private int _questionCount;
+        private int _periodLength;
+        private List<int> _historyList;
+
+        #endregion
+
         public MainForm()
         {
             InitializeComponent();
