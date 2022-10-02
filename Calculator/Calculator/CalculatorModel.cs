87fdfb5ddc34c15c806f788eec0cfd8182b09b94
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-02 02:07:28 +0200

    Create CalculatorModel class and add fields and properties to it

---
+++ Calculator/Calculator/CalculatorModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Calculator
+{
+    /// <summary>
+    /// Művelet felsorolási típusa.
+    /// </summary>
+    public enum Operation { None, Add, Subtract, Multiply, Divide }
+
+    /// <summary>
+    /// Számológép típusa.
+    /// </summary>
+    public class CalculatorModel
+    {
+        #region Private fields
+
+        private double _result;
+        private string _calculationString;
+        private Operation _operation;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Aktuális eredmény lekérdezése.
+        /// </summary>
+        public Double Result { get { return _result; } }
+        /// <summary>
+        /// Aktuális számítás szöveges lekérdezése.
+        /// </summary>
+        public String CalculationString { get { return _calculationString; } }
+
+        #endregion
+    }
+}
