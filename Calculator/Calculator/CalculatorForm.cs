76a75c7540baf4b11d9567db158d764bb2d0d481
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-02 02:19:59 +0200

    Implement KeyDown event handler

--- Calculator/Calculator/CalculatorForm.cs
+++ Calculator/Calculator/CalculatorForm.cs
@@ -32,77 +32,65 @@ namespace Calculator
         #region Event handlers
 
         /// <summary>
-        /// Gomb esemenykezeloje.
+        /// Gomb eseménykezelõje.
         /// </summary>
-        private void Button_Click(object sender, EventArgs e)
+        private void Button_Click(object? sender, EventArgs e)
         {
-            try
+            if (sender is Button button)
             {
-                if (_operation != Operation.None)
+                switch (button.Text)
                 {
-                    Double value = Double.Parse(_textNumber.Text);
-                    switch (_operation)
-                    {
-                        case Operation.Add:
-                            _listHistory.Items.Add(_result + "+" + value + "=" + (_result + value));
-                            _result = _result + value;
-                            break;
-                        case Operation.Substract:
-                            _listHistory.Items.Add(_result + "-" + value + "=" + (_result - value));
-                            _result = _result - value;
-                            break;
-                        case Operation.Multiply:
-                            _listHistory.Items.Add(_result + "*" + value + "=" + (_result * value));
-                            _result = _result * value;
-                            break;
-                        case Operation.Divide:
-                            _listHistory.Items.Add(_result + "/" + value + "=" + (_result / value));
-                            _result = _result / value;
-                            break;
-                    }
-                    _textNumber.Text = _result.ToString();
+                    case "+":
+                        PerformCalculation(Operation.Add);
+                        break;
+                    case "-":
+                        PerformCalculation(Operation.Subtract);
+                        break;
+                    case "*":
+                        PerformCalculation(Operation.Multiply);
+                        break;
+                    case "/":
+                        PerformCalculation(Operation.Divide);
+                        break;
+                    default:
+                        PerformCalculation(Operation.None);
+                        break;
                 }
-                else
-                {
-                    _result = Double.Parse(_textNumber.Text);
-                }
-
-                if (sender is Button button)
-                {
-                    switch (button.Text)
-                    {
-                        case "+": _operation = Operation.Add; break;
-                        case "-": _operation = Operation.Substract; break;
-                        case "*": _operation = Operation.Multiply; break;
-                        case "/": _operation = Operation.Divide; break;
-                        case "=": _operation = Operation.None; break;
-                    }
-                }
-            }
-            catch (OverflowException)
-            {
-                MessageBox.Show("Your input has too many digits!", "Calculation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Your input is not a real number!\nPlease correct!", "Calculation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (NullReferenceException)
-            {
-                MessageBox.Show("No number in input!\nPlease correct!", "Calculation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                _textNumber.Focus();
-                _textNumber.SelectAll();
             }
         }
 
+        /// <summary>
+        /// Billentyû eseménykezelõje.
+        /// </summary>
         private void CalculatorForm_KeyDown(object? sender, KeyEventArgs e)
         {
-
+            switch (e.KeyCode)
+            {
+                case Keys.Add:
+                    PerformCalculation(Operation.Add);
+                    e.SuppressKeyPress = true;
+                    break;
+                case Keys.Subtract:
+                    PerformCalculation(Operation.Subtract);
+                    e.SuppressKeyPress = true;
+                    break;
+                case Keys.Multiply:
+                    PerformCalculation(Operation.Multiply);
+                    e.SuppressKeyPress = true;
+                    break;
+                case Keys.Divide:
+                    PerformCalculation(Operation.Divide);
+                    e.SuppressKeyPress = true;
+                    break;
+                case Keys.Enter:
+                    PerformCalculation(Operation.None);
+                    e.SuppressKeyPress = true;
+                    break;
+            }
         }
 
         #endregion
+
+
     }
 }
