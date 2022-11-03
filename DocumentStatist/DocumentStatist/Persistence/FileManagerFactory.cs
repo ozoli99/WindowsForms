ddc643af9bbaed88e9688f07576d30237f123bfb
Orosz Zoltán <ditp5v@INF.ELTE.HU>
2022-11-03 08:28:48 +0100

    Add FileManagerFactory class

---
+++ DocumentStatist/DocumentStatist/Persistence/FileManagerFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace DocumentStatist.Persistence
+{
+    public class FileManagerFactory
+    {
+        public static IFileManager? CreateForPath(string path) => Path.GetExtension(path) switch
+        {
+            ".txt" => new TxtFileManager(path),
+            ".pdf" => new PdfFileManager(path),
+            _ => null
+        };
+    }
+}
